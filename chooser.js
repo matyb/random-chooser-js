cae46ef0f4b2b29a9c071e4f37be554a1e61ecc9
matyb <mathewkbentley@gmail.com>
2012-04-05 17:46:16 -0400

    refactored mvc into closures

--- chooser.js
+++ chooser.js
@@ -1,25 +1,115 @@
+var model = (function(){
+	var lists = {}, selectedListTitle;
+	return {
+		getSelectedList : function () {
+			return lists[selectedListTitle];
+		},
+		setSelectedListTitle : function (newlySelectedTitle) {
+			selectedListTitle = newlySelectedTitle;
+		},
+		newList : function(listName){
+			lists[value] = [];
+		},
+		isValueInSelectedList : function (value) {
+			var list = getSelectedList();
+			if (list) {
+				for (var j=0;j<list.length;j++) {
+					var listEntry = list[j];
+					if (listEntry == lineText) {
+						return true;
+					}
+				}
+			}
+			return false;
+		}
+	};
+})();
+var view = (function () {
+	var listTitle = "Selected List: ", listsTitle = "Lists:", noListSelected = "<No List Selected>";
+	return {
+		reset : function () {
+			document.getElementById("listTitle").innerHTML = listTitle + noListSelected;
+			document.getElementById("listsTitle").innerHTML = listsTitle + noListSelected;
+			document.getElementById("randomButton").disabled = true;
+		},
+		showTitle : function (newlySelectedTitle) {
+			document.getElementById("listTitle").innerHTML = listTitle + newlySelectedTitle;
+			document.getElementById("randomButton").disabled = false;
+		},
+		enableAddToListButton : function (enable) {
+			document.getElementById("addToListButton").disabled = !enable;
+		},
+		enableAddListButton : function (enabled) {
+			document.getElementById("addListButton").disabled = !enabled;
+		},
+		getCurrentTextValue : function () {
+			return document.getElementById("value").value.trim();
+		},
+		addList : function (onclick) {
+			var lineItem = document.createElement("li");
+			var value = view.getCurrentTextValue();
+			lineItem.appendChild(document.createTextNode(value))
+			lineItem.onclick = onclick;
+			document.getElementById("lists").appendChild(lineItem);
+			document.getElementById("addListButton").disabled = true;
+			return value;
+		}
+	};
+})();
+var controller = (function () {
+	var enableAddToListButton = function () {
+		return model.getSelectedList() != undefined && !model.isValueInSelectedList(value);
+	}
+	var enableAddListButton = function () {
+		// TODO this looks like view code below, could use model instead of document?
+		var value = view.getCurrentTextValue();
+		var lists = document.getElementById("lists");
+		var addListButtonEnabled = true;
+		for (var i=0;i<lists.childNodes.length;i++) {
+			var lineItem = lists.childNodes[i];
+			var lineText = lineItem.innerHTML;
+			if (lineText == value) {
+				addListButtonEnabled = false;
+			}
+		}
+		return addListButtonEnabled;
+	}
+	return {
+		setSelectedListTitle : function (newlySelectedTitle) {
+				model.setSelectedListTitle(newlySelectedTitle);
+				view.showTitle(newlySelectedTitle);
+				view.enableAddToListButton(enableAddToListButton());
+		},
+		valueKeyPressed : function () {
+			var value = view.getCurrentTextValue();
+			view.enableAddListButton(enableAddListButton() && value.length !== 0)
+			view.enableAddToListButton(enableAddToListButton() && value.length !== 0);
+		},
+		addList : function () {
+			var value = view.getCurrentTextValue();
+			model.newList(view.addList(function () {
+				controller.setSelectedListTitle(value);
+			}));
+		}
+	}
+	
+})();
 (function () {
 	"use strict";
 	var doc = window.document, random_chooser = {}, console = window.console || {};
-	random_chooser.lists = {};
-	random_chooser.reset = function () {
-		doc.getElementById("listTitle").innerHTML = random_chooser.listTitle() + random_chooser.noListSelected();
-		doc.getElementById("listsTitle").innerHTML = random_chooser.listsTitle() + random_chooser.noListSelected();
-		doc.getElementById("randomButton").disabled = true;
-	};
 	random_chooser.firstLoad = function () {
 		// initialize logging
 		random_chooser.initConsole();
 		// prepare view
 		doc.getElementById("addListButton").disabled = true;
 		doc.getElementById("addToListButton").disabled = true;
-		random_chooser.reset();
+		view.reset();
 		// attach listeners
 		doc.getElementById("value").onkeyup = function () {
-			random_chooser.valueKeyPressed();
+			controller.valueKeyPressed();
 		};
 		doc.getElementById("addListButton").onclick = function () {
-			random_chooser.addList();
+			controller.addList();
 		};
 		doc.getElementById("addToListButton").onclick = function () {
 			random_chooser.addToList();
@@ -33,36 +123,6 @@
 		console.warn = console.warn || function () {};
 		console.error = console.error || function () {};
 		console.info = console.info || function () {};
-	};
-	random_chooser.valueKeyPressed = function () {
-		var value = doc.getElementById("value").value.trim();
-		var lists = doc.getElementById("lists");
-		var addListButtonDisabled = false;
-		for (var i=0;i<lists.childNodes.length;i++) {
-			var lineItem = lists.childNodes[i];
-			var lineText = lineItem.innerHTML;
-			if (lineText == value) {
-				addListButtonDisabled = true;
-			}
-		}
-		var addToListButtonDisabled = random_chooser.getSelectedList() == undefined || random_chooser.isValueInSelectedList(value);
-		doc.getElementById("addListButton").disabled = addListButtonDisabled || value.length == 0;
-		doc.getElementById("addToListButton").disabled = addToListButtonDisabled || value.length == 0;
-	};
-	random_chooser.getSelectedList = function () {
-		return random_chooser.lists[random_chooser.selected_list_title];
-	};
-	random_chooser.isValueInSelectedList = function(lineText) {
-		var list = random_chooser.getSelectedList();
-		if (list) {
-			for (var j=0;j<list.length;j++) {
-				var listEntry = list[j];
-				if (listEntry == lineText) {
-					return true;
-				}
-			}
-		}
-		return false;
 	};
 	random_chooser.enterPressed = function () {
 		if (!doc.getElementById("randomButton").disabled) {
@@ -78,35 +138,11 @@
 	random_chooser.randomlySelect = function () {
 		console.info("randomly selected");
 	};
-	random_chooser.addList = function () {
-		var lineItem = doc.createElement("li");
-		var valueTextBox = doc.getElementById("value");
-		var value = valueTextBox.value;
-		lineItem.appendChild(doc.createTextNode(value))
-		lineItem.onclick = function () {
-			doc.getElementById("listTitle").innerHTML = random_chooser.listTitle() + value;
-			doc.getElementById("randomButton").disabled = false;
-			random_chooser.selected_list_title = value;
-			doc.getElementById("addToListButton").disabled = random_chooser.getSelectedList() == undefined || random_chooser.isValueInSelectedList(valueTextBox.value);
-		};
-		doc.getElementById("lists").appendChild(lineItem);
-		doc.getElementById("addListButton").disabled = true;
-		var lists = random_chooser.lists[value] = [];
-	};
 	random_chooser.addToList = function () {
-		var selectedList = random_chooser.getSelectedList();
+		var selectedList = model.getSelectedList();
 		var value = doc.getElementById("value").value;
 		selectedList[selectedList.length] = value;
 		doc.getElementById("addToListButton").disabled = true;
-	};
-	random_chooser.listTitle = function () {
-		return "Selected List: ";
-	};
-	random_chooser.listsTitle = function () {
-		return "Lists:";
-	};
-	random_chooser.noListSelected = function () {
-		return "<No List Selected>";
 	};
 	random_chooser.firstLoad();
 })();
