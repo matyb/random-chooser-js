3c78f059e9f7219d369e47de14d19348b7d3d55c
matyb <mathewkbentley@gmail.com>
2012-04-08 04:43:52 -0400

    refactored

--- js/chooser.js
+++ js/chooser.js
@@ -1,26 +1,9 @@
-var randomChooser = {};
+var randomChooser;
+if (!randomChooser) {
+	randomChooser = {};
+}
 randomChooser.model = (function () {
-	var lists = undefined, selectedListName = undefined, db = (function() {
-		var isLocalStorageSupported = false;
-		try {
-			isLocalStorageSupported = 'localStorage' in window && window['localStorage'] !== null;
-		} catch (e) {}
-		if (isLocalStorageSupported) {
-			return window.localStorage;
-		} else {
-			return (function () {
-				var storage = {};
-				return {
-					getItem : function(key) {
-						return storage[key];
-					},
-					setItem : function(key, value) {
-						storage[key] = value;
-					}
-				};
-			})();
-		}
-	})();
+	var lists = undefined, selectedListName = undefined, db = randomChooser.localStorage;
 	lists = db.getItem('random-chooser-lists');
 	if(lists === null || lists === undefined){
 		lists = {};
@@ -70,13 +53,13 @@ randomChooser.model = (function () {
 			var i = 0, list = lists[selectedListName], item = undefined;
 			for (; i<list.length; i++) {
 				item = list[i];
-				if (item == itemName) {
+				if (item === itemName) {
 					list.splice(i,1);
 				}
 			}
 			db.setItem('random-chooser-lists', JSON.stringify(lists));
 		},
-		getSelectedListArray : function () {
+		getSelectedList : function () {
 			return randomChooser.model.getList(selectedListName);
 		},
 		getList : function (listName) {
@@ -84,8 +67,7 @@ randomChooser.model = (function () {
 		}
 	};
 })();
-randomChooser.view = (function () {
-	return {
+randomChooser.view = {
 		redrawLists : function (listNames) {
 			var i = 0, lists = $('#lists');
 			lists.empty();
@@ -151,9 +133,7 @@ randomChooser.view = (function () {
 			$('#itemNameLabel').text(itemName);
 		}
 	};
-})();
-randomChooser.controller = (function () {
-	return {
+randomChooser.controller = {
 		addList : function (listName) {
 			if(randomChooser.model.getList(listName) === undefined){
 				randomChooser.model.addList(listName);
@@ -190,96 +170,12 @@ randomChooser.controller = (function () {
 		enableDisableRandom : function () {
 			randomChooser.view.setRandomDisabled(
 				randomChooser.model.getSelectedListName() === undefined ||
-				randomChooser.model.getSelectedListArray().length <= 0);
+				randomChooser.model.getSelectedList().length <= 0);
 		},
 		selectRandomItem : function () {
-			var randomlySelectedItem = randomChooser.model.getSelectedListArray()[
-				Math.floor(Math.random()*randomChooser.model.getSelectedListArray().length)];
+			var randomlySelectedItem = randomChooser.model.getSelectedList()[
+				Math.floor(Math.random()*randomChooser.model.getSelectedList().length)];
 			randomChooser.view.displayItem(randomlySelectedItem);	
 		}
 	};
-})();
-
-$(document).ready(function () {
-
-});
-$('#firstPage').live('pageinit', function(event) {
-	randomChooser.controller.redrawFirstPage();
-});
-$('#addListPage').live('pageinit', function(event) {
-	var addListOk = $('#addListOk');
-	addListOk.addClass('ui-disabled');
-	$('#listName').keyup(function (event) {
-		var listName = event.currentTarget.value.trim();
-		if(listName.length === 0 || randomChooser.model.getList(listName) !== undefined) {
-			addListOk.addClass('ui-disabled');
-		}else{
-			addListOk.removeClass('ui-disabled');
-		}
-	});
-	addListOk.click(function () {
-		randomChooser.controller.addList( $('#listName').val().trim() );
-	});
-	$("#addListPage").bind('keyup', function(event) {
-		if(event.keyCode == 13  && addListOk[0].className.indexOf('ui-disabled') === -1) {
-			addListOk.click();
-		}
-		return false;
-	});
-});
-$('#addItemPage').live('pageinit', function(event) {
-	var addItemOk = $('#addItemOk');
-	addItemOk.addClass('ui-disabled');
-	$('#itemName').keyup(function (event) {
-		var itemName = event.currentTarget.value.trim();
-		if(itemName.length === 0 || !randomChooser.model.isItemNameUniqueInSelectedList(itemName)) {
-			addItemOk.addClass('ui-disabled');
-		}else{
-			addItemOk.removeClass('ui-disabled');
-		}
-	});
-	addItemOk.click(function () {
-		randomChooser.controller.addItemToSelectedList( $('#itemName').val().trim() );
-	});
-	$("#addItemPage").bind('keyup', function(event) {
-		if(event.keyCode == 13 && addItemOk[0].className.indexOf('ui-disabled') === -1) {
-			addItemOk.click();
-		}
-		return false;
-	});
-});
-$('#viewListPage').live('pageinit', function(event) {
-	$('#random').click(function () {
-		randomChooser.controller.selectRandomItem();
-	});
-});
-$('#viewItemPage').live('pageinit', function(event) {
-	$('#selectAnother').click(function () {
-		randomChooser.controller.selectRandomItem();
-	});
-	$("#viewItemPage").bind('keyup', function(event) {
-		var selectAnother = $('#selectAnother');
-		if(event.keyCode == 13 && selectAnother[0].className.indexOf('ui-disabled') === -1) {
-			selectAnother.click();
-		}
-		return false;
-	});
-});
-$('#viewListPage').live('pagebeforeshow', function(event) {
-	randomChooser.controller.enableDisableRandom();
-	randomChooser.controller.drawSelectedList();
-});
-$('#addListPage').live('pagehide', function(event, ui) {
-	$('#listName').val('');
-	$('#addListOk').addClass('ui-disabled');
-});
-$('#addListPage').live('pageshow', function(event, ui) {
-	$('#listName').focus();
-});
-$('#addItemPage').live('pagehide', function(event, ui) {
-	$('#itemName').val('');
-	$('#addItemOk').addClass('ui-disabled');
-});
-$('#addItemPage').live('pageshow', function(event, ui) {
-	$('#itemName').focus();
-});
+randomChooser.attachPageListeners();
---
+++ js/util.js
@@ -0,0 +1,107 @@
+var randomChooser;
+if (!randomChooser) {
+	randomChooser = {};
+}
+randomChooser.localStorage = (function() {
+		var isLocalStorageSupported = false;
+		try {
+			isLocalStorageSupported = 'localStorage' in window && window['localStorage'] !== null;
+		} catch (e) {}
+		if (isLocalStorageSupported) {
+			return window.localStorage;
+		} else {
+			return (function () {
+				var storage = {};
+				return {
+					getItem : function(key) {
+						return storage[key];
+					},
+					setItem : function(key, value) {
+						storage[key] = value;
+					}
+				};
+			})();
+		}
+	})();
+randomChooser.attachPageListeners = function () {
+	$('#firstPage').live('pageinit', function(event) {
+		randomChooser.controller.redrawFirstPage();
+	});
+	$('#addListPage').live('pageinit', function(event) {
+		var addListOk = $('#addListOk');
+		addListOk.addClass('ui-disabled');
+		$('#listName').keyup(function (event) {
+			var listName = event.currentTarget.value.trim();
+			if(listName.length === 0 || randomChooser.model.getList(listName) !== undefined) {
+				addListOk.addClass('ui-disabled');
+			}else{
+				addListOk.removeClass('ui-disabled');
+			}
+		});
+		addListOk.click(function () {
+			randomChooser.controller.addList( $('#listName').val().trim() );
+		});
+		$("#addListPage").bind('keyup', function(event) {
+			if(event.keyCode === 13  && addListOk[0].className.indexOf('ui-disabled') === -1) {
+				addListOk.click();
+			}
+			return false;
+		});
+	});
+	$('#addItemPage').live('pageinit', function(event) {
+		var addItemOk = $('#addItemOk');
+		addItemOk.addClass('ui-disabled');
+		$('#itemName').keyup(function (event) {
+			var itemName = event.currentTarget.value.trim();
+			if(itemName.length === 0 || !randomChooser.model.isItemNameUniqueInSelectedList(itemName)) {
+				addItemOk.addClass('ui-disabled');
+			}else{
+				addItemOk.removeClass('ui-disabled');
+			}
+		});
+		addItemOk.click(function () {
+			randomChooser.controller.addItemToSelectedList( $('#itemName').val().trim() );
+		});
+		$("#addItemPage").bind('keyup', function(event) {
+			if(event.keyCode === 13 && addItemOk[0].className.indexOf('ui-disabled') === -1) {
+				addItemOk.click();
+			}
+			return false;
+		});
+	});
+	$('#viewListPage').live('pageinit', function(event) {
+		$('#random').click(function () {
+			randomChooser.controller.selectRandomItem();
+		});
+	});
+	$('#viewItemPage').live('pageinit', function(event) {
+		$('#selectAnother').click(function () {
+			randomChooser.controller.selectRandomItem();
+		});
+		$("#viewItemPage").bind('keyup', function(event) {
+			var selectAnother = $('#selectAnother');
+			if(event.keyCode === 13 && selectAnother[0].className.indexOf('ui-disabled') === -1) {
+				selectAnother.click();
+			}
+			return false;
+		});
+	});
+	$('#viewListPage').live('pagebeforeshow', function(event) {
+		randomChooser.controller.enableDisableRandom();
+		randomChooser.controller.drawSelectedList();
+	});
+	$('#addListPage').live('pagehide', function(event, ui) {
+		$('#listName').val('');
+		$('#addListOk').addClass('ui-disabled');
+	});
+	$('#addListPage').live('pageshow', function(event, ui) {
+		$('#listName').focus();
+	});
+	$('#addItemPage').live('pagehide', function(event, ui) {
+		$('#itemName').val('');
+		$('#addItemOk').addClass('ui-disabled');
+	});
+	$('#addItemPage').live('pageshow', function(event, ui) {
+		$('#itemName').focus();
+	});
+} 
