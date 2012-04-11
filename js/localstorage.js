3b93f4badb10a54fb50dca5348e28c5860045387
matyb <mathewkbentley@gmail.com>
2012-04-10 21:37:06 -0400

    fixed bug where app failed silently due to unwriteable local storage

--- js/localstorage.js
+++ js/localstorage.js
@@ -3,11 +3,16 @@ if (!randomChooser) {
 	randomChooser = {};
 }
 randomChooser.win = window;
+randomChooser.alarm = alert;
 randomChooser.createLocalStorage = function () {
   var isLocalStorageSupported = false;
   try {
     isLocalStorageSupported = 'localStorage' in randomChooser.win && randomChooser.win['localStorage'] !== null;
-  } catch (e) {}
+    randomChooser.win['localStorage'].setItem('random-chooser-saveable-test', 'success');
+  } catch (e) {
+    randomChooser.alarm('Random Chooser cannot save lists, you may have private browsing enabled.');
+    isLocalStorageSupported = false;
+  }
   if (isLocalStorageSupported) {
     return randomChooser.win.localStorage;
   } else {
