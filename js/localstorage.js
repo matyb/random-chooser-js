e86d64b254618445bfe59ab30a686cd79a9b81da
matyb <mathewkbentley@gmail.com>
2012-04-10 21:54:26 -0400

    cleanup of bug fix

--- js/localstorage.js
+++ js/localstorage.js
@@ -3,15 +3,18 @@ if (!randomChooser) {
 	randomChooser = {};
 }
 randomChooser.win = window;
-randomChooser.alarm = alert;
+randomChooser.alarm = window.alert;
 randomChooser.createLocalStorage = function () {
   var isLocalStorageSupported = false;
   try {
     isLocalStorageSupported = 'localStorage' in randomChooser.win && randomChooser.win['localStorage'] !== null;
     randomChooser.win['localStorage'].setItem('random-chooser-saveable-test', 'success');
-  } catch (e) {
-    randomChooser.alarm('Random Chooser cannot save lists, you may have private browsing enabled.');
+    if(randomChooser.win['localStorage'].removeItem){ // not used by rest of application
+      randomChooser.win['localStorage'].removeItem('random-chooser-saveable-test', 'success');
+    }
+  } catch (e) {    
     isLocalStorageSupported = false;
+    randomChooser.alarm('Random Chooser cannot save lists, you may have private browsing enabled.');
   }
   if (isLocalStorageSupported) {
     return randomChooser.win.localStorage;
